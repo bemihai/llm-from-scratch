41ae072ec52eaa370179750beb074d2e562ce4ad
bemihai <mihaita.berbec@gmail.com>
2025-01-08 08:42:20 +0200

    add simple tokenizer

--- tokenizer/simple_tokenizer.py
+++ tokenizer/simple_tokenizer.py
@@ -1,3 +1,5 @@
+"""Simple tokenizer that encodes the text into integers based on a provided vocabulary."""
+
 import re
 
 
@@ -8,6 +10,8 @@ def create_vocab(text: str) -> dict[str, int]:
     """
     words = re.split(r'([,.:;?_!"()\']|--|\s)', text)
     words = [item.strip() for item in words if item.strip()]
+    # Add special tokens: end of text, unknown token, etc.
+    words.extend(["<|endoftext|>", "<|unk|>"])
     return {token: idx for idx, token in enumerate(sorted(set(words)))}
 
 
@@ -22,13 +26,15 @@ class SimpleTokenizerV1:
         """Encodes the text into integers."""
         words = re.split(r'([,.:;?_!"()\']|--|\s)', text)
         words = [item.strip() for item in words if item.strip()]
+        # Replace unknown words with <|unk|> token
+        words = [word if word in self.token_to_int else "<|unk|>" for word in words]
         return [self.token_to_int[word] for word in words]
 
     def decode(self, tokens: list[int]) -> str:
         """Decodes the tokens into text."""
         text = ' '.join([self.int_to_token[token] for token in tokens])
         # Replace spaces before the specified punctuations
-        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
         return text
 
 
@@ -40,7 +46,9 @@ if __name__ == '__main__':
     vocab = create_vocab(inputs)
     tokenizer = SimpleTokenizerV1(vocab)
 
-    input_text = "What struck me now was that, for the first time, he resented the tone."
+    text_1 = "What struck me now was that, for the first time, he resented the tone. Evrika!"
+    text_2 = "The verdict was a shock to many, but not to all. The judge was known for his harsh sentences."
+    input_text = " <|endoftext|> ".join([text_1, text_2])
 
     encoded_text = tokenizer.encode(input_text)
     print(f"Encoded: {encoded_text}")
