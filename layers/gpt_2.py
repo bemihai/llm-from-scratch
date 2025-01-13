c72a2ab14c3dd5b50fd4b8f5372523ee94a6c3e5
bemihai <mihaita.berbec@gmail.com>
2025-01-13 09:57:03 +0200

    add gpt-2 architecture

--- layers/gpt_2.py
+++ layers/gpt_2.py
@@ -66,28 +66,25 @@ class GPTModel(nn.Module):
 if __name__ == "__main__":
 
     # tokenize the input text
-    # tokenizer = tiktoken.get_encoding("gpt2")
-    # text_1 = "Every effort moves you"
-    # text_2 = "Every day holds a"
-    # # inputs shape: (batch_size=2, sequence_length=4)
-    # inputs = torch.stack([
-    #     torch.tensor(tokenizer.encode(text_1)),
-    #     torch.tensor(tokenizer.encode(text_2))
-    # ], dim=0)
-    # print(f"Input shape: {inputs.shape}")
-    # print(f"Input tokens: {inputs}")
+    tokenizer = tiktoken.get_encoding("gpt2")
+    text_1 = "Every effort moves you"
+    text_2 = "Every day holds a"
+    # inputs shape: (batch_size=2, sequence_length=4)
+    inputs = torch.stack([
+        torch.tensor(tokenizer.encode(text_1)),
+        torch.tensor(tokenizer.encode(text_2))
+    ], dim=0)
+    print(f"Input shape: {inputs.shape}")
+    print(f"Input tokens: {inputs}")
 
     # create a GPT-2 model with default configuration
     model = GPTModel(Config())
-    # logits = model(inputs)
-    # # output shape: (batch_size=2, sequence_length=4, vocab_size=50257)
-    # print(f"Output shape: {logits.shape}")
-    # print(f"Output logits: {logits}")
+    logits = model(inputs)
+    # output shape: (batch_size=2, sequence_length=4, vocab_size=50257)
+    print(f"Output shape: {logits.shape}")
+    print(f"Output logits: {logits}")
 
     # model summary
-    n_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print(f"Number of parameters: {n_params}")
-
     print(summary(model, [[4]]))
 
 
